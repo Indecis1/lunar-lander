ddbe0e633b7488eb4c4013e9db6e0c823cbd60c1
Yasmine2133 <106029201+Yasmine2133@users.noreply.github.com>
2024-10-25 23:29:37 +0200

    latest functional version

--- src/agent.py
+++ src/agent.py
@@ -5,16 +5,16 @@ import tensorflow as tf
 
 from collections import namedtuple
 from replay_memory import ReplayMemory
-from utils_model import create_model
+from utils_model import build_model
 
 AgentCont = namedtuple("AgentConst", ["buffer_size", "batch_size", "update_every", "gamma", "tau", "input_size", "actions_num"])
 agent_const = AgentCont(
-    2000,
-    64,
+    100000,
+    128,
     5,
-    1,
-    1e-3,
-    (8, ),
+    0.995,
+    0.01,
+    8,
     4
 )
 
@@ -25,11 +25,10 @@ class Agent:
         self.input_size = agent_const.input_size
         self.action_num = agent_const.actions_num
         self.seed = seed
-
-        self.network_to_train = create_model(self.input_size, self.action_num)
-        self.target_network = create_model(self.input_size, self.action_num)
-        self.optimizer = tf.keras.optimizers.Adam()
+        self.optimizer = tf.keras.optimizers.Adam(lr= 0.001)
         self.loss_fn = tf.keras.losses.MeanSquaredError()
+        self.network_to_train = build_model(self.input_size,  self.action_num , self.loss_fn, self.optimizer)
+       
 
         self.memory = ReplayMemory(self.action_num, agent_const.buffer_size, agent_const.batch_size, seed)
 
@@ -38,17 +37,16 @@ class Agent:
     def step(self, obs, action, reward, next_obs, done):
         self.memory.add(obs, action, reward, next_obs, done)
         self.timestep += 1
-        if self.timestep % agent_const.update_every == 0:
-            if len(self.memory) > agent_const.batch_size:
-                experiences = self.memory.sample()
-                self.learn(experiences)
+        if len(self.memory) > agent_const.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
 
     def play_action(self, obs, epsilon):
-        action_logit = self.network_to_train(obs.reshape(1, -1))
+        action_logit = self.network_to_train.predict(obs.reshape(1, -1), verbose=0)
 
         choice = self.greedy_choice(epsilon)
         if choice == "random":
-            return random.choice(np.arange(self.action_num))
+            return np.random.choice(np.arange(self.action_num))
         else:
             return np.argmax(action_logit)
 
@@ -57,45 +55,24 @@ class Agent:
 
     def learn(self, experiences):
         obs, actions, rewards, next_obs, dones = experiences
-        alpha = self.optimizer.get_config()["learning_rate"]
-        with tf.GradientTape() as tape:
-            Q_pred_next = tf.math.reduce_max(self.network_to_train(next_obs, training=True), axis=1)
-            Q_pred = tf.math.reduce_max(self.network_to_train(obs, training=True), axis=1)
-            updated_Q_pred = Q_pred + alpha * (rewards + agent_const.gamma * Q_pred_next - Q_pred)
+        
+        q_val = rewards + agent_const.gamma * np.amax(self.network_to_train.predict_on_batch(next_obs), \
+                                               axis=1) * (1 - dones)
+        target = self.network_to_train.predict_on_batch(obs)
+        idx = np.arange(agent_const.batch_size)
+        target[[idx], [actions]] = q_val
 
-            Q_expected = tf.math.reduce_max(self.target_network(obs, training=True), axis=1)
-            loss_value = self.loss_fn(Q_expected, updated_Q_pred)
-        # Use the gradient tape to automatically retrieve
-        # the gradients of the trainable variables with respect to the loss.
-        grads = tape.gradient(loss_value, self.network_to_train.trainable_weights)
-
-        # Run one step of gradient descent by updating
-        # the value of the variables to minimize the loss.
-        self.optimizer.apply_gradients(zip(grads, self.network_to_train.trainable_weights))
-
-        self.soft_update(agent_const.tau)
-
-    def soft_update(self, tau: float):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-        :param local_model: tf model where weights will be copied from
-        :param target_model: tf model where weights will be copied to
-        :param tau: interpolation parameter
-        """
-        local_layer_weights = self.network_to_train.get_weights()
-        target_layer_weights = self.target_network.get_weights()
-        for i in range(len(target_layer_weights)):
-            target_layer_weights[i] = tau * local_layer_weights[i] + (1.0-tau) * target_layer_weights[i]
-        self.target_network.set_weights(target_layer_weights)
+        self.network_to_train.fit(obs, target, epochs=1, verbose=0)
+       
 
     def save_weights(self, save_dir: str = ""):
         if save_dir != "":
             self.network_to_train.save_weights(os.path.join(save_dir, "network_to_train.weights.h5"))
-            self.target_network.save_weights(os.path.join(save_dir, "target_network.weights.h5"))
+
 
     @staticmethod
-    def load_ai_model_weights(load_path_dir: str):
-        model = create_model(agent_const.input_size, agent_const.actions_num)
-        model.load_weights(os.path.join(load_path_dir, "network_to_train.weights.h5"))
+    def load_ai_model_weights(self,load_path_dir: str):
+        model =  build_model(self.input_size,  self.action_num , self.loss_fn, self.optimizer)
+        model.load_weights(os.path.join(load_path_dir, "target_network.weights.h5"))
         return model
         
--- src/main.py
+++ src/main.py
@@ -32,34 +32,41 @@ def plot_reward(num_episode, avg_reward, title, save_plot_dir):
     plt.show()
 
 
-def training(agent: Agent, num_episode: int = 1000, epsilon_start: float = 1.0, epsilon_min: float = 0.01, epsilon_decay_rate: float = 0.997, save_plot_dir:str = "", save_dir: str = ""):
-    env = gym.make('LunarLander-v3')
+def training(agent: Agent, num_episode: int = 1000, epsilon_start: float = 1.0, epsilon_min: float = 0.01, epsilon_decay_rate: float = 0.999,seed=0, save_plot_dir:str = "", save_dir: str = ""):
+    env = gym.make('LunarLander-v2')
+    env.action_space.seed(seed)
     epsilon = epsilon_start
     avg_rewards_per_episode = []
     # num_iter_before_printing = num_episode // 20
     print("/n")
     for episode in tqdm.tqdm(range(num_episode)):
         obs, _ = env.reset()
-        avg_rewards = 0
-        i = 0
+        rewards = 0
+        max_steps = 1000
         # for timestep in range(max_timestep):
-        while True:
+        for step in range(max_steps):
             action = agent.play_action(obs, epsilon)
             new_obs, reward, done, inf, _ = env.step(action)
             agent.step(obs, action, reward, new_obs, done)
             obs = new_obs
-            avg_rewards += reward
-            i += 1
-            if done:
-                # i = timestep + 1
+            rewards += reward
+            epsilon = max(epsilon_min, epsilon * epsilon_decay_rate)
+            if done or step ==  max_steps-1 :
+                print(f'Episode: {episode} | Steps: {step} | Total reward: {rewards} \
+                    | Epsilon: {epsilon}')
                 break
-        avg_rewards /= i
-        avg_rewards_per_episode.append(avg_rewards)
-        epsilon = max(epsilon_min, epsilon * epsilon_decay_rate)
-        # if (episode+1) % num_iter_before_printing == 0:
-        #     print("\n episode: {}, Average reward: {}".format(episode+1, avg_rewards))
+        #avg_rewards /= i
+        avg_rewards_per_episode.append(rewards)
+    
+        if episode % 10 == 0:
+            print(f'Last 10 episodes mean reward: {np.mean(avg_rewards_per_episode[-10:])}')
+        if np.mean(avg_rewards_per_episode[-100:]) >= 200:
+                print('*** Environment solved ***')
+                break
+            
     avg_rewards_per_episode = np.array(avg_rewards_per_episode)
-    plot_reward(num_episode, avg_rewards_per_episode, "Avg Reward per episode", save_plot_dir)
+    plot_reward(episode, avg_rewards_per_episode, "Avg Reward per episode", save_plot_dir)
+    
     if save_dir != "":
         # save_path = "./models"
         create_path_if_not_exist(save_dir)
@@ -68,7 +75,7 @@ def training(agent: Agent, num_episode: int = 1000, epsilon_start: float = 1.0, 
 
 
 def model_evaluation(load_path_dir):
-    env = gym.make('LunarLander-v3', render_mode='human')
+    env = gym.make('LunarLander-v2', render_mode='human')
     # env.metadata["render_fps"] = 180
     obs, _ = env.reset()
     model = Agent.load_ai_model_weights(load_path_dir)
@@ -83,6 +90,7 @@ def model_evaluation(load_path_dir):
 
 
 if __name__ == "__main__":
-    agent = Agent(42)
-    training(agent, num_episode=1000, save_plot_dir="plots/", save_dir="models/")
-    model_evaluation("models/")
+    agent = Agent(0)
+    training(agent, num_episode= 400, save_plot_dir="plots/", save_dir="models/")
+    for i in range(20):
+        model_evaluation("models/")
